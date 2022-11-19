7e51134c95a70128484783aaabc0f618baeb77d0
Ismar Santos <ismarpp.santos@gmail.com>
2022-11-19 18:50:23 -0300

    commit aula python

---
+++ aula3.py
@@ -0,0 +1,31 @@
+#variáveis
+
+# nome_completo = 'João Medeiros'
+# idade = 48
+# altura = 1.98
+# peso = 98.00
+
+
+##Operadores matemáticos
+
+divisao = 10 / 2 ## com uma barra o resultado sempre será um número float, mesmo que a divisão seja com números inteiros
+
+print(divisao)
+
+divisao_inteira = 5.55 // 7 # resultado aproximado, arredondado.
+print(divisao_inteira)
+
+exponeciacao = 2 ** 3 # é igual a 2³
+print(exponeciacao)
+
+modulo = 50 % 2 ## resto da divisão
+
+print(modulo)
+
+concatenacao = 'Joao' + ' ' + 'Paulo'
+
+print(concatenacao)
+
+repeticao = 'a' * 25 ## repetirá a letra a 25 vezes
+
+print(repeticao)
