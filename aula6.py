17f794ca0884c41f0e274e480dba5a474b12f4b9
Ismar Santos <ismarpp.santos@gmail.com>
2022-11-19 19:04:03 -0300

    condições if elif else

---
+++ aula6.py
@@ -0,0 +1,12 @@
+# if/ elif      / else
+# se/ se não se / se não
+
+
+condicao = input('Você está bem hoje?')
+
+if condicao == 's':
+    print('Que bom!')
+elif condicao == 'n':
+    print('espero que melhore :)')
+else:
+    print('Eu realmente prciso saber.')    
