eb967ca6147ca17f7e35147c822458e932eaa3b9
Ismar Santos <ismarpp.santos@gmail.com>
2022-11-21 20:03:57 -0300

    operadores e condicoes

--- aula6.py
+++ aula6.py
@@ -2,11 +2,20 @@
 # se/ se não se / se não
 
 
-condicao = input('Você está bem hoje?')
+# condicao = input('Você está bem hoje?')
 
-if condicao == 's':
-    print('Que bom!')
-elif condicao == 'n':
-    print('espero que melhore :)')
+# if condicao == 's':
+#     print('Que bom!') ##é necessário essa endentação para a condição funcionar
+# elif condicao == 'n':
+#     print('espero que melhore :)')
+# else:
+#     print('Eu realmente prciso saber.')    
+
+# a única condição das 3 que pode vir só é o IF as demais dependem umas das outras.
+
+condicao_2 = False
+
+if condicao_2:
+    print('se for true passa')
 else:
-    print('Eu realmente prciso saber.')    
+    print('é falsa')
