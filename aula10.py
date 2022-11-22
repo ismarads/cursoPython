f3a65adc747e7085b2180d3eab1bdff71f134725
Ismar Santos <ismarpp.santos@gmail.com>
2022-11-21 21:47:35 -0300

    continuação do curso

---
+++ aula10.py
@@ -0,0 +1,34 @@
+# Operadores in e not in 
+# Strings em Python são iteráveis
+
+# nome = 'Ismar'
+
+# # print(nome[3])
+# # print(nome[1])
+
+# print('w' in nome)  # tem a letra w na variável nome ou w está em nome?
+# print('mar' in nome)
+# print(10*'*')
+# print('inho' not in nome)
+# print('Is' not in nome)
+"""
+nome = 'Maria Carmo'
+
+if ' ' in nome:
+    print(f' O nome {nome} tem espaços')
+else:
+    print(f'O nome {nome} não tem espaços')    
+
+"""
+numero = 10
+
+if numero > 1:
+    if numero > 2:
+        if numero > 3:
+            print('Numero maior que 3')
+        else:
+                print('Numero menor que 3')
+    else:
+            print('Numero menot que 2')
+else:
+    print('Numero menor que 1')
