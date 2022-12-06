403d0978c4ccddebdbf311b876d03ddb1f22d7d1
Ismar Santos <ismarpp.santos@gmail.com>
2022-12-05 21:03:52 -0300

    f-strings

---
+++ aula11.py
@@ -0,0 +1,15 @@
+"""
+Interpolação básica
+s - srign
+d e i - int
+f - float
+x e X hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Luiz'
+preco = 1234.6867843534
+
+variavel = '%s, o preço é R$%.2f' % (nome,preco)
+
+print(variavel)
+print('O valor de %d é %08X' % (1556,1556)) #pode ser x ou X
---
+++ aula12.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+variavel = 'ABC'
+
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{-1000.345345345345345:,.1f}')
+print(f'o hexa decimal de 1700 é {1700:08x}')
